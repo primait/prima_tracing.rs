533506fc78fd4ebe4e6cc5cc1b17bd9abf7c9803
Mite Ristovski <mite.ristovski@hotmail.com>
2022-03-11 08:46:35 +0100

    Use OtelData from span extensions to extract trace and span ids for the datadog logger

--- src/json/formatter.rs
+++ src/json/formatter.rs
@@ -140,20 +140,30 @@ impl EventFormatter for DefaultEventFormatter {
         {
             use opentelemetry::trace::TraceContextExt;
             use std::collections::HashMap;
+            use tracing_opentelemetry::OtelData;
 
-            let current_ctx = opentelemetry::Context::current();
-            let current_span = current_ctx.span();
-            let span_ctx = current_span.span_context();
+            if let Some(current_span) = ctx.current_span().id().and_then(|id| ctx.span(id)) {
+                let ext = current_span.extensions();
+                if let Some(otel_data) = ext.get::<OtelData>() {
+                    let builder = &otel_data.builder;
 
-            // Datadog trace IDs need to be 64 bits long
-            let trace_id = u128::from_be_bytes(span_ctx.trace_id().to_bytes()) as u64;
-            let span_id = u64::from_be_bytes(span_ctx.span_id().to_bytes());
+                    let parent_span = &otel_data.parent_cx.span();
+                    let parent_span_ctx = parent_span.span_context();
 
-            let mut dd = HashMap::new();
-            dd.insert("trace_id", trace_id);
-            dd.insert("span_id", span_id);
+                    let span_id = builder.span_id.unwrap_or(parent_span_ctx.span_id());
+                    let trace_id = builder.trace_id.unwrap_or(parent_span_ctx.trace_id());
 
-            map_serializer.serialize_entry("dd", &dd)?;
+                    // Datadog trace and span IDs need to be 64-bit unsigned integers
+                    let trace_id = u128::from_be_bytes(trace_id.to_bytes()) as u64;
+                    let span_id = u64::from_be_bytes(span_id.to_bytes());
+
+                    let mut dd = HashMap::new();
+                    dd.insert("trace_id", trace_id);
+                    dd.insert("span_id", span_id);
+
+                    map_serializer.serialize_entry("dd", &dd)?;
+                }
+            }
         }
 
         map_serializer.end()?;
