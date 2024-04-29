5c40282568963e251ae60af122984eef6e78256d
Oliver Browne <109075559+oliverbrowneprima@users.noreply.github.com>
2024-04-29 10:01:14 +0200

    [PLATFORM-1698]: Support automatic extraction of K8S env vars for traces and metrics (#99)
    * Add new layer to add env vars to span attributes
    
    * lint

--- src/subscriber.rs
+++ src/subscriber.rs
@@ -31,6 +31,7 @@ pub fn configure_subscriber<T: EventFormatter + Send + Sync + 'static>(
                 version: _config.version.clone(),
             })
             .with(crate::layer::ErrorLayer)
+            .with(crate::layer::KubeEnvLayer::default())
     };
 
     #[cfg(not(feature = "json-logger"))]
@@ -41,6 +42,7 @@ pub fn configure_subscriber<T: EventFormatter + Send + Sync + 'static>(
         use crate::json::storage::PrimaJsonStorage;
         subscriber
             .with(PrimaJsonStorage)
+            .with(crate::layer::KubeEnvLayer::default())
             .with(PrimaFormattingLayer::new(
                 _config.service.clone(),
                 _config.country.to_string(),
