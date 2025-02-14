341a9eb174e7fd88beb7f14eda15f6b53c4f0a70
mae <26093674+MaeIsBad@users.noreply.github.com>
2025-02-14 16:02:59 +0100

    Add report_error macro

--- src/lib.rs
+++ src/lib.rs
@@ -35,3 +35,34 @@ pub use crate::config::{
 pub use crate::subscriber::{
     configure_subscriber, init_subscriber, ContextInfo, EventFormatter, Tracing, Uninstall,
 };
+
+/// Create a tracing error event, casting the error to &dyn [std::error::Error] for [layer::ErrorLayer],
+/// and adding the type name as error.kind.
+///
+/// Usage:
+/// ```
+/// use prima_tracing::report_error;
+///
+/// let error = "not a number".parse::<usize>().unwrap_err();
+/// report_error!(&error, "Parsing error!");
+/// ```
+///
+/// You can also add use add attributes and do things, just like with a regular [tracing::error]
+/// macro call
+/// ```
+/// use prima_tracing::report_error;
+/// # let input = "not a number";
+/// # let uid = "1223";
+///
+/// let error = input.parse::<usize>().unwrap_err();
+/// report_error!(&error, input, user=uid, "Parsing error: {}", &error);
+/// ```
+#[macro_export]
+macro_rules! report_error {
+    ($error:expr, $($args:tt)*) => {
+        {
+          let kind = ::std::any::type_name_of_val(&$error);
+          ::tracing::error!(error.kind = kind, error = $error as &dyn ::std::error::Error, $($args)+)
+        }
+    };
+}
