b226819ac7f7dd0abf3760ee7ae3d7f70c051460
Cristiano Piemontese <cristiano.piemontese@prima.it>
2025-10-31 11:24:36 +0100

    refactor: try fixing kube_env

--- src/layer/kube_env.rs
+++ src/layer/kube_env.rs
@@ -1,6 +1,6 @@
 use opentelemetry::KeyValue;
 use tracing::{span, Subscriber};
-use tracing_opentelemetry::OtelData;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 use tracing_subscriber::{layer::Context, Layer};
 
 // Extracts KUBE_APP_PART_OF, KUBE_APP_MANAGED_BY, KUBE_APP_VERSION and KUBE_APP_INSTANCE from the environment
@@ -29,22 +29,32 @@ where
 {
     fn on_new_span(&self, _attrs: &span::Attributes<'_>, id: &span::Id, ctx: Context<'_, S>) {
         let span = ctx.span(id).expect("Span not found, this is a bug");
-        let mut extensions = span.extensions_mut();
+        let mut exts = span.extensions_mut();
 
-        if let Some(otel_data) = extensions.get_mut::<OtelData>() {
-            let builder = &mut otel_data.builder;
-            if let Some(ref mut attributes) = builder.attributes {
-                if let Some(ref part_of) = self.kube_app_part_of {
-                    attributes.push(KeyValue::new("kube_app_part_of", part_of.clone()));
-                }
-                if let Some(ref managed_by) = self.kube_app_managed_by {
-                    attributes.push(KeyValue::new("kube_app_managed_by", managed_by.clone()));
-                }
-                if let Some(ref version) = self.kube_app_version {
-                    attributes.push(KeyValue::new("kube_app_version", version.clone()));
-                }
-                if let Some(ref instance) = self.kube_app_instance {
-                    attributes.push(KeyValue::new("kube_app_instance", instance.clone()));
+        let mut kvs: Vec<KeyValue> = Vec::new();
+        if let Some(ref part_of) = self.kube_app_part_of {
+            kvs.push(KeyValue::new("kube_app_part_of", part_of.clone()));
+        }
+        if let Some(ref managed_by) = self.kube_app_managed_by {
+            kvs.push(KeyValue::new("kube_app_managed_by", managed_by.clone()));
+        }
+        if let Some(ref version) = self.kube_app_version {
+            kvs.push(KeyValue::new("kube_app_version", version.clone()));
+        }
+        if let Some(ref instance) = self.kube_app_instance {
+            kvs.push(KeyValue::new("kube_app_instance", instance.clone()));
+        }
+
+        // Store attributes to apply on enter
+        exts.insert(KubeAttrs(kvs));
+    }
+
+    fn on_enter(&self, id: &span::Id, ctx: Context<'_, S>) {
+        if let Some(span_ref) = ctx.span(id) {
+            if let Some(attrs) = span_ref.extensions().get::<KubeAttrs>() {
+                let current = tracing::Span::current();
+                for kv in &attrs.0 {
+                    current.set_attribute(kv.key.clone(), kv.value.clone());
                 }
             }
         }
